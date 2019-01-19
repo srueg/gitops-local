e81bf6dfdcfcd1c2c1bdc0c7f52dd5fca25a10b3
Simon Rüegg <simon@rueggs.ch>
2019-01-19 18:29:58 +0100

    Improve bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ set -o pipefail
 set -o nounset
 #set -o xtrace
 
-function apply {
+function kapply {
     kubectl "$@" --save-config --dry-run --output yaml | kubectl apply -f -
 }
 
@@ -13,23 +13,28 @@ export TILLER_NAMESPACE=tiller
 export FLUX_FORWARD_NAMESPACE=flux
 export GIT_REPO='git@github.com:srueg/gitops-local.git'
 
-apply create namespace $TILLER_NAMESPACE
-apply create clusterrolebinding tiller-admin --clusterrole cluster-admin --serviceaccount $TILLER_NAMESPACE:default
+kapply create namespace $TILLER_NAMESPACE
+kapply create clusterrolebinding tiller-admin --clusterrole cluster-admin --serviceaccount $TILLER_NAMESPACE:default
 
-helm init --wait --tiller-namespace $TILLER_NAMESPACE
+helm init \
+  --tiller-namespace $TILLER_NAMESPACE \
+  --override 'spec.template.spec.containers[0].command'='{/tiller,--storage=secret}' \
+  --history-max 10 \
+  --wait
 
 kubectl apply -f https://raw.githubusercontent.com/weaveworks/flux/master/deploy-helm/flux-helm-release-crd.yaml
 
 helm repo add weaveworks https://weaveworks.github.io/flux
+helm repo update
 
-helm install --name flux \
+helm install weaveworks/flux \
+  --name flux \
   --set helmOperator.create=true \
   --set helmOperator.createCRD=false \
   --set helmOperator.tillerNamespace=$TILLER_NAMESPACE \
   --set git.url=$GIT_REPO \
   --namespace $FLUX_FORWARD_NAMESPACE \
-  --wait \
-  weaveworks/flux
+  --wait
 
 if hash fluxctl 2>/dev/null; then
     fluxctl identity
